1dfaa9624320048217860dac6092a3aee1a3c22d
何依濠 <yihao.he@jcu.edu.au>
2021-11-28 02:53:42 +0800

    Type ** according to the length of the password, the function was not used the first time, and the function was used the second time

---
+++ prac_03/password_check.py
@@ -0,0 +1,30 @@
+def version_1():
+    MIN_LENGTH = 5
+    MAX_LENGTH = 15
+    password = input("Enter a password with a minimum of 5 and a maximum of 15: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Password is too short, please try again: ")
+        while len(password) > MAX_LENGTH:
+            password = input("Password is too long, please try again: ")
+
+    print('*' * len(password))
+
+
+version_1()
+
+def main():
+    MIN_LENGTH = 5
+    MAX_LENGTH = 15
+    get_password(MAX_LENGTH, MIN_LENGTH)
+
+
+def get_password(MAX_LENGTH, MIN_LENGTH):
+    password = input("Enter a password with a minimum of 5 and a maximum of 15: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Password is too short, please try again: ")
+        while len(password) > MAX_LENGTH:
+            password = input("Password is too long, please try again: ")
+    print('*' * len(password))
+
+
+main()
