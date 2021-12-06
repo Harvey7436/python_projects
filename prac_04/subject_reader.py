0b26e932f97c27302d5d3c6d83e16341a0a1a009
何依濠 <yihao.he@jcu.edu.au>
2021-12-06 16:40:38 +0800

    This is the changed code, the LIST function is added, and the content is put into the list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,11 +9,18 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subject_details(data)
+
+
+def display_subject_details(data):
+    for i in data:
+        print("{} is taught by {} and has {} students".format(i[0], i[1], i[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_list = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +29,12 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subject_list.append(parts)
         print("----------")
+
+    return subject_list
     input_file.close()
+
 
 
 main()
