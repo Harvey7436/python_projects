795f85b12d9596b9cbb9f7e5608d718d26d67cb3
何依濠 <yihao.he@jcu.edu.au>
2022-01-11 04:51:56 +0800

    The random is put into the class, and the car can only be driven if the numbers meet certain conditions...

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,7 +1,6 @@
 from prac_08.car import Car
 import random
 
-
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -9,7 +9,6 @@ def main():
         print("Attempting to drive {}km:".format(i))
         print("{:12} drove {:2}km".format(good_car.name, good_car.drive(i)))
         print("{:12} drove {:2}km".format(bad_car.name, bad_car.drive(i)))
-    # print the final states of the cars
     print(good_car)
     print(bad_car)
 
